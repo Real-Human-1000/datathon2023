5ca26a846fbda27e68291a9b583ee0ad6591eeca
arshjain08 <arsharshj@gmail.com>
2023-01-27 22:50:57 -0600

    pog

--- pandastest.py
+++ pandastest.py
@@ -1,5 +1,7 @@
 import requests
-
+import pandas as pd
+import numpy as np
+"""
 download_url = "https://raw.githubusercontent.com/fivethirtyeight/data/master/nba-elo/nbaallelo.csv"
 target_csv_path = "nba_all_elo.csv"
 
@@ -8,7 +10,15 @@ response.raise_for_status()    # Check that the request was successful
 with open(target_csv_path, "wb") as f:
     f.write(response.content)
 print("Download ready.")
+"""
 
 import pandas as pd
-nba = pd.read_csv("Investment_Data_Train (1).csv")
-print(nba)
+df = pd.read_csv("Investment_Data_Train (1).csv")
+new_df = df[['MSN', 'StateCode', 'Year', 'Amount',
+       'CO2 Emissions (Mmt)', 'TotalNumberofInvestments',
+       'TotalAmountofAssistance']]
+
+states_df = new_df[~new_df['StateCode'].isin(['DC','US','X3','X5'])]
+print(states_df)
+states_df2 = states_df[states_df['Year'] == 2015]
+print(states_df2)
