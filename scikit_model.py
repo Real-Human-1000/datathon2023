aad488d9c799485c80de4d0bd0ca9a63de742a29
real-human-1000 <1henryprendergast@gmail.com>
2023-01-28 13:27:58 -0600

    Scikit Model updated

--- scikit_model.py
+++ scikit_model.py
@@ -8,7 +8,7 @@ from time import sleep
 
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import (LinearRegression, MultiTaskLassoCV, RidgeCV)
+from sklearn.linear_model import (LinearRegression, Lasso, RidgeCV)
 from sklearn.metrics import mean_squared_error
 
 # Referenced https://towardsdatascience.com/build-better-regression-models-with-lasso-271ce0f22bd
@@ -27,9 +27,15 @@ def get_tables_for_year(year):
     Metrics_df = states_df2[['StateCode', 'CO2 Emissions (Mmt)', 'TotalNumberofInvestments', 'TotalAmountofAssistance']]
     Metrics_df = Metrics_df.drop_duplicates(subset=None, keep="first", inplace=False)
     Metrics_df.set_index('StateCode', inplace=True)
-    # Final_df = pd.concat([MSN_df, Metrics_df], axis="columns")
 
     return MSN_df, Metrics_df
+
+
+def reorganize_msn_metrics(msn, metrics):
+    data = msn.assign(CO2Emissions=metrics[['CO2 Emissions (Mmt)']])
+    data = data.assign(TotalInvestments=metrics[['TotalNumberofInvestments']])
+    output = metrics[['TotalAmountofAssistance']]
+    return data, output
 
 
 def plot_correlation(df):
@@ -43,27 +49,30 @@ def plot_correlation(df):
 
 
 msn2015, metrics2015 = get_tables_for_year(2015)
+data2015, out2015 = reorganize_msn_metrics(msn2015, metrics2015)
+
 msn2016, metrics2016 = get_tables_for_year(2016)
+data2016, out2016 = reorganize_msn_metrics(msn2016, metrics2016)
 
 # print(msn2015)
 # print(msn2016)
 
-# plot_correlation(msn2015)
+# plot_correlation(data2015)
 
-linear_regression = make_pipeline(StandardScaler(), LinearRegression())
-linear_regression.fit(msn2015, metrics2015)
-
-mse = mean_squared_error(metrics2016, linear_regression.predict(msn2016))
-print(mse)
+# linear_regression = make_pipeline(StandardScaler(), LinearRegression())
+# linear_regression.fit(data2015, out2015)
+#
+# mse = mean_squared_error(out2016, linear_regression.predict(data2016))
+# print(mse)
 
 # linear_regression_coef = linear_regression[-1].coef_
 
 # print(list(linear_regression_coef))
 
 
-lasso_cv = make_pipeline(StandardScaler(), MultiTaskLassoCV())
-lasso_cv.fit(msn2015, metrics2015)
+lasso_model = make_pipeline(StandardScaler(), Lasso(tol=0.0001, max_iter=1000))
+lasso_model.fit(data2015, out2015)
 
-mse = mean_squared_error(metrics2016, lasso_cv.predict(msn2016))
-print(mse)
+mse = mean_squared_error(out2015, lasso_model.predict(data2015))
+print("{:e}".format(mse))
 
