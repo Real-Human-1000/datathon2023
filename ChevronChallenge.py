5302ce39198028717c53fa56c6427475fc960a41
arshjain08 <arsharshj@gmail.com>
2023-01-27 21:36:00 -0600

    pog

--- ChevronChallenge.py
+++ ChevronChallenge.py
@@ -1,3 +1,5 @@
 import pandas as pd
 nba = pd.read_csv("Investment_Data_Train (1).csv")
 print(nba)
+
+#helloworld
--- pandastest.py
+++ pandastest.py
@@ -2,7 +2,7 @@ import requests
 
 download_url = "https://raw.githubusercontent.com/fivethirtyeight/data/master/nba-elo/nbaallelo.csv"
 target_csv_path = "nba_all_elo.csv"
-#ArshJain123
+
 response = requests.get(download_url)
 response.raise_for_status()    # Check that the request was successful
 with open(target_csv_path, "wb") as f:
